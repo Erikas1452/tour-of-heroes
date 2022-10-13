ccf9fca4e5dda53f507df9cf050625fd4a2522ad
Erikas1452 <erikastumanovas@gmail.com>
2022-10-13 13:42:28 +0300

    logged-in user cannot access registration and login pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,10 +11,12 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'register',
     component: RegisterComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'heroes',
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import {
+  ActivatedRouteSnapshot,
   CanActivate,
   Router,
+  RouterStateSnapshot,
 } from '@angular/router';
 import { AuthService } from '../services/auth-service/auth.service';
 
@@ -9,12 +11,31 @@ import { AuthService } from '../services/auth-service/auth.service';
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(public auth: AuthService, public router: Router) {}
+  private routeURL!: string;
 
-  public canActivate(): boolean {
-    if (!this.auth.isAuthenticated()) {
-      this.router.navigate(['login']);
+  constructor(public _auth: AuthService, public _router: Router) {}
+
+  public canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+
+
+    //User is not authenticated and trying to use login and register routes
+    if (!this._auth.isAuthenticated() && (state.url === '/login' || state.url === '/register')) {
+      return true;
+    }
+
+    //User is not authenticated and trying to acess any other route
+    if(!this._auth.isAuthenticated()){
+      this._router.navigate(['login']);
       return false;
+    }
+    
+    //User is authenticated and trying to use login and register routes
+    if (this._auth.isAuthenticated() && (state.url === '/login' || state.url === '/register'))
+    {
+      this._router.navigate(['dashboard']);
     }
     return true;
   }
