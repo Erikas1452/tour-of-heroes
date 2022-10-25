beb9936c75d61f9059b04ae3578adc2ac8731c2b
Erikas1452 <erikastumanovas@gmail.com>
2022-10-25 13:55:04 +0300

    added OnPush strategy to HeroEditFormDialog Component

--- src/app/components/pop-ups/hero-edit-form-dialog/hero-edit-form-dialog.component.ts
+++ src/app/components/pop-ups/hero-edit-form-dialog/hero-edit-form-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import {
   DialogData,
@@ -9,6 +9,7 @@ import {
   selector: 'app-hero-edit-form-dialog',
   templateUrl: './hero-edit-form-dialog.component.html',
   styleUrls: ['./hero-edit-form-dialog.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeroEditFormDialogComponent implements OnInit {
   constructor(
