1b15b6e14132969d541b1e78002a7fc030f63d93
Erikas1452 <erikastumanovas@gmail.com>
2022-10-25 13:54:04 +0300

    added OnPush strategy to HeroesComponent

--- src/app/components/heroes-page/heroes/heroes.component.ts
+++ src/app/components/heroes-page/heroes/heroes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { Hero } from 'src/app/common/hero';
 import { MatDialog } from '@angular/material/dialog';
@@ -14,6 +14,7 @@ import { User } from 'src/app/common/user';
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeroesComponent implements OnInit {
   public heroes$: Observable<Hero[]> = this.store.select(HeroState.selectHeroes);
