ed6dc13f8a835e6a7788a6d652d907091d911801
Erikas1452 <erikastumanovas@gmail.com>
2022-10-26 14:24:03 +0300

    added roles to users

---
+++ src/app/common/role.ts
@@ -0,0 +1,5 @@
+export enum Role {
+    Visitor = 'visitor',
+    User = 'user',
+    Admin = 'admin'
+}
--- src/app/common/user.ts
+++ src/app/common/user.ts
@@ -1,5 +1,8 @@
+import { Role } from "./role";
+
 export interface User {
     id: number;
     email: string;
+    role: Role;
 }
   
