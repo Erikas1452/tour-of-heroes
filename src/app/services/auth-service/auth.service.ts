282b43fc02365cec163b3977d546bec783e8706c
Erikas1452 <erikastumanovas@gmail.com>
2022-10-26 14:28:20 +0300

    added admin login and added role check for regular user login to auth service

--- src/app/services/auth-service/auth.service.ts
+++ src/app/services/auth-service/auth.service.ts
@@ -1,14 +1,32 @@
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
-import { tap } from 'rxjs';
+import { catchError, tap } from 'rxjs';
+import { ErrorHandler } from 'src/app/common/ErrorHandler';
+import { Role } from 'src/app/common/role';
+import { SnackbarHandler } from 'src/app/common/SnackBarHandler';
 import { UserService } from '../user-service/user.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  public adminLogin(username: string, password: string) {
+    return this.userService.login(username, password).pipe(
+      tap((response) => {
+        if (response.user.role !== Role.Admin) {
+          this._snackbarHandler.openSnackBar('Only Admin is allowed to access this');
+          throw new Error('Only Admin is allowed to access this');
+        }
+        localStorage.setItem('access_token', response.accessToken);
+      })
+    );
+  }
 
-  constructor(private userService: UserService, public jwtHelper: JwtHelperService) {}
+  constructor(
+    private _snackbarHandler: SnackbarHandler, 
+    private userService: UserService,
+    public jwtHelper: JwtHelperService
+  ) {}
 
   public isAuthenticated(): boolean {
     const token = localStorage.getItem('access_token');
@@ -27,8 +45,12 @@ export class AuthService {
   public userLogin(username: string, password: string) {
     return this.userService.login(username, password).pipe(
       tap((response) => {
+        if (response.user.role !== Role.Visitor && response.user.role !== Role.User) {
+          this._snackbarHandler.openSnackBar('Only regular users and visitors are allowed to access this');
+          throw new Error('Only regular users and visitors are allowed to access this');
+        }
         localStorage.setItem('access_token', response.accessToken);
       })
-    )
+    );
   }
 }
